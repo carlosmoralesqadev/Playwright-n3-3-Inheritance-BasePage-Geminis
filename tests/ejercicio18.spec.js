61ca25bc03caf9878ec15d831becefa8cb0d3d8d
carlosmoralesqadev <carlosmoralesqadev@gmail.com>
2026-04-18 19:17:45 -0300

    Retomando

---
+++ tests/ejercicio18.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import { Locators } from './archivo_locators.js';
+import { LoginPage } from '../pages/LoginPage.js';
+
+test.describe('Funcionalidad', () => {
+    /** @type {LoginPage} */
+    let pageObject;
+
+    test.beforeEach(async ({ page }) => {
+        pageObject = new LoginPage(page);
+        await page.goto(Locators.page);
+    });
+
+    test('Caso de prueba', async () => {
+        //Retomando
+        
+    });
+});
